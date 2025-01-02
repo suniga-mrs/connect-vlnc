60431b9e97b251183bfe7ee6c022b1cf989d11be
Michelle Suniga <suniga.mrs@gmail.com>
2025-01-02 21:02:27 +0800

    55eea54

---
+++ assets/use-app-layout-4DHEedsF.js
@@ -0,0 +1,1 @@
+import{r}from"./index-BykOfNXl.js";const e=r(!1),a=r(!1),o=r(!1),s=()=>e.value=!e.value,t=()=>{a.value=!1};function f(){return{formDrawerFullScreen:a,formDrawerOpen:e,sideBarMiniState:o,formDrawerToggle:s,resetFormDrawer:t}}export{f as u};
